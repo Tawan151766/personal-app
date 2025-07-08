d6caacd0dafe635624c118c108c05f951bc9e576
Tawan123 <tawan.ch@waylar.net>
2025-07-08 11:00:22 +0700

    fix: bug display skills

--- src/components/SkillsList.js
+++ src/components/SkillsList.js
@@ -2,7 +2,7 @@ import SkillIcon from "./SkillIcon";
 
 export default function SkillsList({ skills = [] }) {
   return (
-    <div className="flex gap-4 p-3 lg:p-6 flex-wrap pr-4 lg:pr-6 justify-center lg:justify-start">
+    <div className="flex  gap-4 p-3 lg:p-6 flex-wrap pr-4 lg:pr-6 justify-start mb-15 lg:mb-0">
       {skills.map((skill, index) => (
         <SkillIcon key={index} skill={skill} />
       ))}
