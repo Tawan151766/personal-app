cdf718c60470259f44e1c5796334d12ec579293e
Tawan123 <tawan.ch@waylar.net>
2025-07-09 03:27:47 +0700

    feat: avatar

--- src/app/about-me/page.jsx
+++ src/app/about-me/page.jsx
@@ -39,7 +39,7 @@ function AboutPage() {
   return (
     <AboutLayout onBackClick={handleBackClick} onShareClick={handleShareClick}>
       <ProfileCard
-        profileImage="https://lh3.googleusercontent.com/aida-public/AB6AXuDLXBcQ91w2vZJiPAyIU_x8rnptFE9c8_fS5hBzn-UL2Dze85S5VInwv9wy3pTbtbgeiQ_7ohWN9yimtIBm12mTZALoyl2ecIKC1inHbviEuivo_RjCbinF7dssjr3G0A0DIJ7620l6N9G2E_A_w5_fcoPh5sAv0IWwc8La-XdX-AGpqomGnEOTKvLXh2vNr9A5Ql-N8qjSUHH-fNlUzTU3vSemsHZaVJKRuXH-SeohnpKX5OHC6anjEI9Z-igAaPlwN86o0pjnLGal"
+        profileImage="/assets/avatar.png"
         name="Ethan Carter"
         title="Full-Stack Developer"
         experience="2 years of experience"
